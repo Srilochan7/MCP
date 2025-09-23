7c2e490cfb097704c472b2aeca80427699466dd7
Srilochan <srilochan7@gmail.com>
2025-09-23 23:06:30 +0530

    changes

---
+++ http/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from http!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ http/server.py
@@ -0,0 +1,16 @@
+from mcp.server.fastmcp import FastMCP 
+
+
+mcp = FastMCP("server")
+
+
+
+@mcp.tool()
+def greeting(name: str) -> str:
+    "Send a greeting"
+    
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
